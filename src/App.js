8e272ef3e1a93dc7c6e095b35f17731c49ad883f
maconrad <mconrad@Marios-MacBook-Pro.local>
2020-03-19 10:14:21 +0100

    MenuItem withRouting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 import Homepage from './pages/homepage/Homepage.comp'
 import './App.css';
 
+const HatsPage =(props) => {
+  console.log(props)
+  //props.match.params.hatId
+  //Option 1 via Link
+  //Option 2 via History prop
+  //Option 3 via props.match.url = dynamic Routes
+  return (
+  <div>
+    <h1>Hats Page</h1>
+    <Link to='/'>Home</Link>
+    <Link to={`${props.match.url}/13`}>To number 13</Link>
+    <button onClick={() => props.history.push('/')} >Back</button>
+  </div>
+)}
+
 function App() {
+  // exact={false} (or without the exact)
+  // localhost:3000/hats  -> Homepage matches and both render 
+  // <Route path='/hats/:hatId' component={HatsDetails} />
   return (
     <div>
-      <Homepage></Homepage>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={HatsPage} />
+        
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.comp.jsx
+++ src/components/menu-item/menu-item.comp.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+//Higher Order Component = HOC
+// =Essentially a function that takes a component as an argument
+// and returns a modified component
+import { withRouter } from 'react-router-dom'; 
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => {
   return (
-      <div className={`${size} menu-item`}>
+      <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} ></div>
         <div className="content">
           <h1 className="title">{title.toUpperCase()}</h1>
@@ -14,4 +18,7 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+// Power up a component -> access to location, match, history
+//  instead of tunneling these props all the way down from homepage
+//  to directory to menu-item (bad design)
+export default withRouter(MenuItem);
